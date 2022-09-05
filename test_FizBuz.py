2ecc59e294243e002d008de0a8090a57dfc57968
Paliwalmahesh <paliwalmahesh981@gmail.com>
2022-09-05 10:02:33 +0530

    Final test of code

--- test_FizBuz.py
+++ test_FizBuz.py
@@ -7,7 +7,10 @@ class TestFizBuz(unittest.TestCase):
     def test_Print_Multiple_of_3_5(self):
         self.assertEqual(FizBuz.fizzBuzz(3),[1,2,"Fizz"])
         self.assertEqual(FizBuz.fizzBuzz(5),[1, 2, 'Fizz', 4, 'Buzz'])
-    
+        self.assertEqual(FizBuz.fizzBuzz(15),[1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'Fizz'])
+        self.assertEqual(FizBuz.fizzBuzz(20),[1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'Fizz', 16, 17, 'Fizz', 19, 'Buzz'])
+
+
 
         
 
