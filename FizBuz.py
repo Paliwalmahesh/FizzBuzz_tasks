ef596189ee518e534add838c8479bee490591881
Paliwalmahesh <paliwalmahesh981@gmail.com>
2022-09-05 09:57:40 +0530

    Third Test of Multiple of 3 & 5

--- FizBuz.py
+++ FizBuz.py
@@ -3,6 +3,8 @@ def fizzBuzz(n):
     for i in range(1,n+1):
         if i%3 == 0 and i>=3:
             l1.append("Fizz")
+        elif i%5 == 0 and i>=5:
+            l1.append("Buzz")
         else:
             l1.append(i)
     return l1
--- test_FizBuz.py
+++ test_FizBuz.py
@@ -4,9 +4,10 @@ class TestFizBuz(unittest.TestCase):
     def test_fizzBuzz(self):
         self.assertEqual(FizBuz.fizzBuzz(2),[1, 2])
         self.assertEqual(FizBuz.fizzBuzz(1),[1])
-    def test_Print_Multiple_of_3(self):
+    def test_Print_Multiple_of_3_5(self):
         self.assertEqual(FizBuz.fizzBuzz(3),[1,2,"Fizz"])
-        
+        self.assertEqual(FizBuz.fizzBuzz(5),[1, 2, 'Fizz', 4, 'Buzz'])
+    
 
         
 
