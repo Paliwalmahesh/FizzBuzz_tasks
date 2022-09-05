c5e76174d80c394ab544d14234622c0514afa326
Paliwalmahesh <paliwalmahesh981@gmail.com>
2022-09-05 09:54:46 +0530

    second Test and code Updating

--- FizBuz.py
+++ FizBuz.py
@@ -1,5 +1,8 @@
 def fizzBuzz(n):
     l1=[]
     for i in range(1,n+1):
-        l1.append(i)
+        if i%3 == 0 and i>=3:
+            l1.append("Fizz")
+        else:
+            l1.append(i)
     return l1
--- test_FizBuz.py
+++ test_FizBuz.py
@@ -4,6 +4,11 @@ class TestFizBuz(unittest.TestCase):
     def test_fizzBuzz(self):
         self.assertEqual(FizBuz.fizzBuzz(2),[1, 2])
         self.assertEqual(FizBuz.fizzBuzz(1),[1])
+    def test_Print_Multiple_of_3(self):
+        self.assertEqual(FizBuz.fizzBuzz(3),[1,2,"Fizz"])
+        
+
+        
 
       
        
